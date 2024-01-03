53bcf3a08711f709853646d7e48e17f33f554da8
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-01-03 23:17:19 +0700

    add content of projects page and add the animation on the content

--- js/script.js
+++ js/script.js
@@ -107,3 +107,19 @@ listItems.forEach(function (li) {
     this.classList.add("active");
   });
 });
+
+// Project Event
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+const removeActiveClasses = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
